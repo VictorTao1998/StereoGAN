d0a389fa578425292d1dda9f64e5013e19f8c470
VictorTao1998 <jit124@ucsd.edu>
2021-10-17 15:25:15 -0700

    a

--- train.py
+++ train.py
@@ -387,7 +387,7 @@ def train(args,cfg):
                     writer.add_image('BAB_R/imgB', rightB_visual, i)
                     writer.add_image('BAB_R/fakeA', fakeA_R_visual, i)
                     writer.add_image('BAB_R/recB', recB_R_visual, i)
-                    writer.add_image('pred/gt_disp', dispA.detach().cpu(), i)
+                    writer.add_image('pred/gt_disp', dispA[0].detach().cpu(), i)
                     writer.add_image('pred/pred_disp', disp_ests[0][0].detach().cpu(), i)
 
                 if args.lambda_warp_inv:
