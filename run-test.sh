2df93e0e307b1fec6ba2f8a5503aae029ec134fe
VictorTao1998 <jit124@ucsd.edu>
2021-10-21 13:37:05 -0700

    test

--- run-test.sh
+++ run-test.sh
@@ -4,15 +4,15 @@ export PYTHONWARNINGS="ignore"
 now=$(date +"%Y%m%d_%H%M%S")
 model_name="StereoGAN"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /cephfs/jianyu/StereoGAN/test.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/test.py \
 --print_freq=220 \
---output '/cephfs/jianyu/eval/StereoGAN_test' \
+--output '/jianyu-fast-vol/eval/StereoGAN_test' \
 --use_multi_gpu=1 \
 --maxdisp=192 \
---config-file '/cephfs/jianyu/StereoGAN/configs/remote_test_gan.yaml' \
+--config-file '/jianyu-fast-vol/StereoGAN/configs/remote_test_gan.yaml' \
 --exclude-bg \
 --exclude-zeros \
 --load_from_mgpus_model 1 \
---load_dispnet_path '/cephfs/jianyu/eval/StereoGAN_train_batch5/checkpoints/StereoGAN/ep1_D1_0.5722_EPE7.7740.pth.rar' \
---load_gan_path '/cephfs/jianyu/eval/StereoGAN_train_batch5/checkpoints/StereoGAN/ep1_D1_0.5722_EPE7.7740.pth.rar' \
+--load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train/checkpoints/StereoGAN/ep0_D1_0.7201_EPE10.8228.pth.rar' \
+--load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train/checkpoints/StereoGAN/ep0_D1_0.7201_EPE10.8228.pth.rar' \
 --load_checkpoints 1
