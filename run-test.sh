e063f49a2ee7bfc9b5b4c99e92fc6073db56ca8b
VictorTao1998 <jit124@ucsd.edu>
2021-11-03 14:10:29 -0700

    bug

--- run-test.sh
+++ run-test.sh
@@ -6,13 +6,14 @@ model_name="StereoGAN"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/test.py \
 --print_freq=220 \
---output '/jianyu-fast-vol/eval/StereoGAN_test_sim' \
+--output '/jianyu-fast-vol/eval/StereoGAN_test_final' \
 --use_multi_gpu=1 \
 --maxdisp=192 \
 --config-file '/jianyu-fast-vol/StereoGAN/configs/remote_test_gan_v10.yaml' \
 --exclude-bg \
 --exclude-zeros \
 --load_from_mgpus_model 1 \
---load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train/checkpoints/StereoGAN/ep2_D1_0.2991_EPE5.4978.pth.rar' \
---load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train/checkpoints/StereoGAN/ep2_D1_0.2991_EPE5.4978.pth.rar' \
---load_checkpoints 1
+--load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
+--load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
+--load_checkpoints 1 \
+--onReal
