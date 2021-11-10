6bd2252357fb8eb2edca7229dcbff45e53540a94
VictorTao1998 <jit124@ucsd.edu>
2021-11-09 16:45:52 -0800

    a

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/train.py \
 --save_interval=5 \
 --print_freq=10 \
 --checkpoint_save_path="/jianyu-fast-vol/eval/StereoGAN_train_final_1/checkpoints/${model_name}" \
---writer='/jianyu-fast-vol/eval/StereoGAN_train_final_1' \
+--writer='/jianyu-fast-vol/eval/StereoGAN_train_final_2' \
 --use_multi_gpu=1 \
 --maxdisp=192 \
 --lambda_corr=1 \
@@ -22,7 +22,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/train.py \
 --lambda_warp_inv=5 \
 --lambda_disp_warp_inv=5 \
 --config-file '/jianyu-fast-vol/StereoGAN/configs/remote_train_gan_v10.yaml' \
---load_from_mgpus_model 1 \
---load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
---load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
---load_checkpoints 1
+#--load_from_mgpus_model 1 \
+#--load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
+#--load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train_final/checkpoints/StereoGAN/ep1_D1_0.7821_EPE11.8211.pth.rar' \
+#--load_checkpoints 1
