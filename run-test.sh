cb513bec52bf7dda4d1fc0b81eaa0df01d25a0dc
VictorTao1998 <jit124@ucsd.edu>
2021-11-11 16:25:30 -0800

    test

--- run-test.sh
+++ run-test.sh
@@ -6,7 +6,7 @@ model_name="StereoGAN"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/test.py \
 --print_freq=220 \
---output '/jianyu-fast-vol/eval/StereoGAN_test_f_real_include' \
+--output '/jianyu-fast-vol/eval/StereoGAN_test_fo_real_exclude' \
 --use_multi_gpu=1 \
 --maxdisp=192 \
 --config-file '/jianyu-fast-vol/StereoGAN/configs/remote_test_gan_v10.yaml' \
@@ -15,4 +15,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/StereoGAN/test.py \
 --load_dispnet_path '/jianyu-fast-vol/eval/StereoGAN_train_final_1/checkpoints/StereoGAN/ep4_D1_0.5868_EPE8.5989.pth.rar' \
 --load_gan_path '/jianyu-fast-vol/eval/StereoGAN_train_final_1/checkpoints/StereoGAN/ep4_D1_0.5868_EPE8.5989.pth.rar' \
 --load_checkpoints 1 \
+--exclude-zeros \
 --onReal
